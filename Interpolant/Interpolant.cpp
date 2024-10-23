6a7f91ad9cbd015907e732c90b3e8c984a5c9ad7
Egor Kapustin <114067435+fakeplastictree11@users.noreply.github.com>
2024-10-24 01:41:02 +0300

    Fixed realization issue with ans=0

--- Interpolant/Interpolant.cpp
+++ Interpolant/Interpolant.cpp
@@ -32,8 +32,8 @@ class NewtonInterpolator {
     //Интерполяция
     yType interpolate(const xType& x) const noexcept {
         yType ans, multiplication;
-        ans = 0;
-        for (int i = 0; i < N; i++) {
+        ans = differences[0];
+        for (int i = 1; i < N; i++) {
             multiplication = 1;
             for (int j = 0; j < i; j++) {
                 multiplication *= x - points[j];
